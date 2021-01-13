1da34c8043a584a15473ed5181f79b1b368a9502
Scott Thomson <scott.thomson@tngtech.com>
2021-01-13 21:01:12 +0100

    Tell app its commit ID.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM openjdk:11
+ARG COMMIT_ID
 
 COPY build/libs/*jar  app.jar
 
+ENV COMMIT_ID ${COMMIT_ID}
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/userlarsb/demo/SimpleController.java
+++ src/main/java/com/userlarsb/demo/SimpleController.java
@@ -11,8 +11,12 @@ public class SimpleController {
 	@RequestMapping("/")
 	public String index() {
 		Random rnd = new Random();
+		String version = System.getenv("COMMIT_ID");
+		version = version != null ? version : "MISSING";
 		int randInt = rnd.nextInt(100000);
-		return "The application is running. Random output: " + randInt;
+		return "The application is running. " +
+				"Commit ID is " + version + ". " +
+				"Random output: " + randInt;
 	}
 
 }
